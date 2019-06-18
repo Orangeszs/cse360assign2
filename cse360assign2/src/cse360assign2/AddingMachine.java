86b6968b123db837ae62e62201b90c29ebfaca02
Thais Herve <therve@asu.edu>
2019-06-18 12:40:51 -0700

    Add files via upload

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,65 @@
+
+package cse360assign2;
+
+/**
+ * 
+ * @author thais
+ * 
+ *
+ */
+
+public class AddingMachine {
+	
+	private int total;
+	
+	public AddingMachine () {
+	
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	/**
+	 * Not implemented yet
+	 * @return 0
+	 */
+	public int getTotal () {
+	
+		return 0;
+	
+	}
+	
+	/**
+	 * Not implemented yet
+	 * @param value
+	 */
+	public void add (int value) {
+	
+	}
+	
+	
+	/**
+	 * Not implemented yet
+	 * @param value
+	 */
+	public void subtract (int value) {
+	
+	}
+	
+	
+	/**
+	 * Not implemented yet
+	 * @return ""
+	 */
+	public String toString () {
+		return "";
+	
+	}
+	
+	
+	/**
+	 * Not implemented yet
+	 */
+	public void clear() {
+	
+	}
+}
